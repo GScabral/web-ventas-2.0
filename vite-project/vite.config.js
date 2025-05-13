60f0f51c2ed855394f7fd19c8e615b2ae8e448d1
Dylan Badaro <89850931+DylanBadaro@users.noreply.github.com>
2025-05-13 20:47:14 -0300

    url del front de render

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Cambiado a @vitejs/plugin-react
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Usa el plugin correcto para React
   server: {
-    port: 3005,
-    open: true,
-  }
-})
+    host: '0.0.0.0', // Asegúrate de tener esto para el despliegue
+    port: 3005,       // El puerto que estés usando
+    allowedHosts: ['https://amoremio.onrender.com'], // Añade tu dominio de Render aquí
+  },
+});
