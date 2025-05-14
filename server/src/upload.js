4170dc48d054978244e4308f3f793230da26bb71
Dylan Badaro <89850931+DylanBadaro@users.noreply.github.com>
2025-05-14 16:25:22 -0300

    upload.js

--- server/src/upload.js
+++ server/src/upload.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('./utils/cludinary'); 
+const cloudinary = require('./utils/cloudinary'); 
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
