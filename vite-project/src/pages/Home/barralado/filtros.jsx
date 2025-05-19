494deecdcd4a0e80f447e36cf270eaa010f6cb4d
GScabral <126419509+GScabral@users.noreply.github.com>
2025-05-19 11:18:42 -0300

    Update filtros.jsx

--- vite-project/src/pages/Home/barralado/filtros.jsx
+++ vite-project/src/pages/Home/barralado/filtros.jsx
@@ -10,8 +10,6 @@ const FiltrosSidebar = () => {
     const [precio, setPrecio] = useState(false);
     const [selectedCategory, setSelectedCategory] = useState("");
     const [selectedPriceOrder, setSelectedPriceOrder] = useState("");
-    const [selectedSubcategory, setSelectedSubcategory] = useState("");
-    const [showSubcategories, setShowSubcategories] = useState(true); 
     const [isMobile, setIsMobile] = useState(window.innerWidth < 800);
 
     const allProductos = useSelector((state) => state.allProductos);
@@ -25,34 +23,19 @@ const FiltrosSidebar = () => {
 
     const toggleFiltros = () => {
         setMostrarF(!mostrarF);
-        setShowSubcategories(true); 
     };
 
     const toggleOrden = () => {
         setMostrarO(!mostrarO);
     };
 
-    const handleFilter = (category, subcategory) => {
-        let filteredProducts = allProductos;
-
-        if (!subcategory && selectedCategory !== category) {
-            setSelectedCategory(category);
-            setSelectedSubcategory("");
-            dispatch(filterProduc({ categoria: category, subcategoria: "", allProductos: filteredProducts }));
-        } else if (!subcategory && selectedCategory === category) {
+    const handleFilter = (category) => {
+        if (selectedCategory === category) {
             setSelectedCategory("");
-            setSelectedSubcategory("");
             dispatch(filterProduc({ categoria: "", subcategoria: "", allProductos }));
-        } else if (subcategory) {
-            filteredProducts = filteredProducts.filter(producto =>
-                producto.categoria && producto.categoria.toLowerCase() === category.toLowerCase()
-            );
-            filteredProducts = filteredProducts.filter(producto =>
-                producto.subcategoria && producto.subcategoria.toLowerCase() === subcategory.toLowerCase()
-            );
+        } else {
             setSelectedCategory(category);
-            setSelectedSubcategory(subcategory);
-            dispatch(filterProduc({ categoria: category, subcategoria: subcategory, allProductos: filteredProducts }));
+            dispatch(filterProduc({ categoria: category, subcategoria: "", allProductos }));
         }
     };
 
@@ -76,78 +59,54 @@ const FiltrosSidebar = () => {
                 FILTRAR POR:
             </button>
             {mostrarF && (
-                <div>
-                    <ul className="ul-filtros">
-                        {Object.entries(categoria).map(([categoriaPrincipal, subcategorias]) => (
-                            <div key={categoriaPrincipal}>
-                                <button
-                                    className={selectedCategory === categoriaPrincipal ? "button-selected" : "button-talles"}
-                                    onClick={() => handleFilter(categoriaPrincipal, "")}
-                                >
-                                    {categoriaPrincipal}
-                                </button>
-                                {showSubcategories && selectedCategory === categoriaPrincipal && (
-                                    <ul>
-                                        {subcategorias && subcategorias.map((subcategoria) => (
-                                                <li key={subcategoria}>
-                                                    <button
-                                                        className={selectedSubcategory === subcategoria ? "button-selected" : "button-sub-categoria"}
-                                                        onClick={() => handleFilter(categoriaPrincipal, subcategoria)}
-                                                    >
-                                                        {subcategoria}
-                                                    </button>
-                                                </li>
-                                            ))
-                                        ) : (
-                                            <li className="no-subcategoria">Sin subcategorías</li>
-                                        )}
-                                    </ul>
-                                )}
-                            </div>
-                        ))}
-                    </ul>
-                </div>
+                <ul className="ul-filtros">
+                    {Object.keys(categoria).map((categoriaPrincipal) => (
+                        <li key={categoriaPrincipal}>
+                            <button
+                                className={selectedCategory === categoriaPrincipal ? "button-selected" : "button-talles"}
+                                onClick={() => handleFilter(categoriaPrincipal)}
+                            >
+                                {categoriaPrincipal}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
             )}
+
             <hr className="divider" />
+
             <button className="button-filtros" onClick={toggleOrden}>
                 ORDENAR POR:
             </button>
             {mostrarO && (
-                <div>
-                    <ul className="ul-filtros">
-                        <button className="button-filtros" onClick={() => setPrecio(!precio)}>
-                            Precio
-                        </button>
-                        {precio && (
-                            <div>
-                                <button
-                                    className={selectedPriceOrder === "precioAsc" ? "button-selected" : "button-talles"}
-                                    onClick={() => handleOrder("precioAsc")}
-                                >
-                                    Menor a Mayor
-                                </button>
-                                <button
-                                    className={selectedPriceOrder === "precioDesc" ? "button-selected" : "button-talles"}
-                                    onClick={() => handleOrder("precioDesc")}
-                                >
-                                    Mayor a Menor
-                                </button>
-                            </div>
-                        )}
-                    </ul>
-                </div>
+                <ul className="ul-filtros">
+                    <button className="button-filtros" onClick={() => setPrecio(!precio)}>
+                        Precio
+                    </button>
+                    {precio && (
+                        <div>
+                            <button
+                                className={selectedPriceOrder === "precioAsc" ? "button-selected" : "button-talles"}
+                                onClick={() => handleOrder("precioAsc")}
+                            >
+                                Menor a Mayor
+                            </button>
+                            <button
+                                className={selectedPriceOrder === "precioDesc" ? "button-selected" : "button-talles"}
+                                onClick={() => handleOrder("precioDesc")}
+                            >
+                                Mayor a Menor
+                            </button>
+                        </div>
+                    )}
+                </ul>
             )}
+
             {isMobile && (
                 <div className="links-container">
-                    <Link to="/">
-                        <button className="superior-barra">Inicio</button>
-                    </Link>
-                    <Link to="/DevolucionCambio">
-                        <button className="superior-barra">Cambio/Devolución</button>
-                    </Link> 
-                    <Link to="/comoPagar">
-                        <button className="superior-barra">Venta por mayor</button>
-                    </Link>
+                    <Link to="/"><button className="superior-barra">Inicio</button></Link>
+                    <Link to="/DevolucionCambio"><button className="superior-barra">Cambio/Devolución</button></Link>
+                    <Link to="/comoPagar"><button className="superior-barra">Venta por mayor</button></Link>
                 </div>
             )}
         </div>
@@ -155,4 +114,3 @@ const FiltrosSidebar = () => {
 };
 
 export default FiltrosSidebar;
-
