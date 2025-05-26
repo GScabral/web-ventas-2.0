82df8bf7be6b0d5b6bd9a297eccbb90ea46c3d7c
GScabral <126419509+GScabral@users.noreply.github.com>
2025-05-26 10:12:37 -0300

    Update home.jsx

--- vite-project/src/pages/Home/home.jsx
+++ vite-project/src/pages/Home/home.jsx
@@ -26,6 +26,9 @@ const Home = () => {
   useEffect(() => {
     dispatch(getProductos());
   }, [dispatch]);
+    useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [paginaActual]);
 
   useEffect(() => {
     const handleResize = () => {
