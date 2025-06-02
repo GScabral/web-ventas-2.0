6919f45b8750aae8985fc805a73d88f5b889770e
GScabral <126419509+GScabral@users.noreply.github.com>
2025-06-02 17:22:39 -0300

    Update Nav.jsx

--- vite-project/src/pages/Home/Nav/Nav.jsx
+++ vite-project/src/pages/Home/Nav/Nav.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";Add commentMore actions
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { getProductos } from "../../../redux/action";
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { getProductos, buscar } from "../../../redux/action";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import FiltrosSidebar from "../barralado/filtros";
 import "./Nav.css";
@@ -16,6 +17,12 @@ const Nav = () => {
   const [selectedSubcategory, setSelectedSubcategory] = useState("");
   const [selectedPriceOrder, setSelectedPriceOrder] = useState("");
 
+  const handleSearchChange = (e) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    dispatch(buscar(value));
+  };
+
   const handleClearSearch = () => {
     setSearchTerm("");
     dispatch(getProductos());
@@ -24,28 +31,44 @@ const Nav = () => {
   return (
     <nav className="back-nav">
       <div className="nav-content">
+        {/* Botón de categorías */}
         <button
           className="menu-button"
           onClick={() => setShowCategories(!showCategories)}
-          {searchTerm && <button onClick={handleClearSearch}>X</button>}
-        </div>
-
-        {/* ✅ Botón del carrito */}
-        <button className="carrito-nav">
-          🛒
-          {/* <span className="cart-counter">2</span> // Descomentar si querés contador */}
+        >
+          Categorías
         </button>
+
+        {/* Input de búsqueda */}
+        {window.location.pathname !== '/carrito' && (
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Buscar productos..."
+              value={searchTerm}
+              onChange={handleSearchChange}
+              className="barra-buscar"
+            />
+            {searchTerm && (
+              <button onClick={handleClearSearch} className="clear-search-btn">
+                X
+              </button>
+            )}
+          </div>
+        )}
+
+        {/* Botón del carrito */}
+        {window.location.pathname !== '/carrito' && (
+          <Link to="/carrito">
+            <button className="carrito-nav">
+              <FontAwesomeIcon icon={faShoppingCart} />
+              <span className="cart-counter"></span>
+            </button>
+          </Link>
+        )}
       </div>
 
-       {window.location.pathname !== '/carrito' && (
-                    <Link to="/carrito">
-                        <button className="carrito-nav">
-                            <FontAwesomeIcon icon={faShoppingCart} />
-                            <span className="cart-counter"></span> {/* Contador del carrito */}
-                        </button>
-                    </Link>
-                )}
-      {/* Menú de filtros (dropdown-style) */}
+      {/* Menú de filtros */}
       {showCategories && (
         <div className="dropdown-menu show">
           <FiltrosSidebar
