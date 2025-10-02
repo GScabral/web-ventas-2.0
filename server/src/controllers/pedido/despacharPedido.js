85ceec3a8d39b00c90ea1d2be41e14f69052cb10
GsCabral <cabralgonzalo10@gmail.com>
2025-10-02 10:42:36 -0300

    detallesDespacho

--- server/src/controllers/pedido/despacharPedido.js
+++ server/src/controllers/pedido/despacharPedido.js
@@ -1,25 +1,25 @@
 const { DetallesPedido } = require('../../db');
 
 const despacharDetalle = async (req, res) => {
-  try {
-    const { idDetalle } = req.params;
+    try {
+        const { idDetalle } = req.params;
 
-    // Buscar el detalle por su clave primaria
-    const detalle = await DetallesPedido.findByPk(idDetalle);
+        // Buscar el detalle por su clave primaria
+        const detalle = await DetallesPedido.findByPk(idDetalle);
 
-    if (!detalle) {
-      return res.status(404).json({ message: 'Detalle no encontrado' });
+        if (!detalle) {
+            return res.status(404).json({ message: 'Detalle no encontrado' });
+        }
+
+        // Cambiar el estado del detalle
+        detalle.estado_pedido = 'DESPACHADO';
+        await detalle.save();
+
+        res.json({ message: 'Detalle despachado correctamente', detalle });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al despachar el pedido' });
     }
-
-    // Cambiar el estado del detalle
-    detalle.estado_pedido = 'DESPACHADO';
-    await detalle.save();
-
-    res.json({ message: 'Detalle despachado correctamente', detalle });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error al despachar el pedido' });
-  }
 };
 
-module.exports = { despacharDetalle };
+module.exports = despacharDetalle;
